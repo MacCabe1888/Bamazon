6486959221036a1db62e31ebc7f5f384cdd427fb
Cameron <cameronjmaccabe@gmail.com>
2019-03-27 21:52:47 -0700

    fixed id-name mismatch bug

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -54,67 +54,56 @@ function customerPrompt(res) {
     ]).then(function(inquirerResponse) {
       console.log();
       id = parseInt(inquirerResponse.chosenID);
-      name = res[id - 1].product_name;
-      inquirer
-        .prompt([
-          {
-            type: "input",
-            name: "quantity",
-            message: `Please enter the quantity of "${name}" you would like to order:`
-          }
-        ]).then(function(inquirerResponse) {
-          console.log("\nChecking the storage room...\n");
-          connection.query("SELECT stock_quantity FROM products WHERE ?",
-            {
-              item_id: id
-            },
-            function(err, res) {
-              if (err) throw err;
-              let orderQuantity = inquirerResponse.quantity;
-              let supply = res[0].stock_quantity;
-              if (orderQuantity > supply) {
-                if (supply === 0) {
-                  inquirer
-                    .prompt([
-                      {
-                        type: "confirm",
-                        name: "resetDisplay",
-                        message: `Unfortunately, we are out of stock of "${name}." Would you like to order something else?`,
-                        default: false
-                      }
-                    ]).then(function(inquirerResponse) {
-                      if (inquirerResponse.resetDisplay === true) {
-                        displayItems();
-                      } else {
-                        connection.end();
-                      }
-                    })
-                } else if (supply === 1) {
-                  console.log(`Unfortunately, we have but one unit of "${name}" remaining in stock. Please try your order again.`);
-                  displayItems();
-                } else {
-                  console.log(`Unfortunately, we have only ${supply} units of "${name}" remaining in stock. Please try your order again.`);
-                  displayItems();
-                }
-              } else {
-                connection.query("UPDATE products SET stock_quantity = stock_quantity - " + orderQuantity + " WHERE ?",
-                  {
-                    item_id: id
-                  },
-                  function(err) {
-                    if (err) throw err;
-                  }
-                );
-                connection.query("SELECT price FROM products WHERE ?",
-                  {
-                    item_id: id
-                  },
-                  function(err, res) {
-                    if (err) throw err;
-                    let unitPrice = res[0].price;
-                    // The total cost of the user's purchase is equal to the number of units ordered times the price per unit. Multiplying and then dividing by 100 ensures that the operations are performed on integers, thus avoiding the potential imprecision of floating-point operations.
-                    let totalCost = (100 * orderQuantity * unitPrice) / 100;
-                    connection.query(`UPDATE products SET product_sales = product_sales + ${totalCost} WHERE ?`,
+      connection.query("SELECT product_name FROM products WHERE ?",
+        {
+          item_id: id
+        },
+        function (err, res) {
+          if (err) throw err;
+          name = res[0].product_name;
+          inquirer
+            .prompt([
+              {
+                type: "input",
+                name: "quantity",
+                message: `Please enter the quantity of "${name}" you would like to order:`
+              }
+            ]).then(function(inquirerResponse) {
+              console.log("\nChecking the storage room...\n");
+              connection.query("SELECT stock_quantity FROM products WHERE ?",
+                {
+                  item_id: id
+                },
+                function(err, res) {
+                  if (err) throw err;
+                  let orderQuantity = inquirerResponse.quantity;
+                  let supply = res[0].stock_quantity;
+                  if (orderQuantity > supply) {
+                    if (supply === 0) {
+                      inquirer
+                        .prompt([
+                          {
+                            type: "confirm",
+                            name: "resetDisplay",
+                            message: `Unfortunately, we are out of stock of "${name}." Would you like to order something else?`,
+                            default: false
+                          }
+                        ]).then(function(inquirerResponse) {
+                          if (inquirerResponse.resetDisplay === true) {
+                            displayItems();
+                          } else {
+                            connection.end();
+                          }
+                        })
+                    } else if (supply === 1) {
+                      console.log(`Unfortunately, we have but one unit of "${name}" remaining in stock. Please try your order again.`);
+                      displayItems();
+                    } else {
+                      console.log(`Unfortunately, we have only ${supply} units of "${name}" remaining in stock. Please try your order again.`);
+                      displayItems();
+                    }
+                  } else {
+                    connection.query("UPDATE products SET stock_quantity = stock_quantity - " + orderQuantity + " WHERE ?",
                       {
                         item_id: id
                       },
@@ -122,20 +111,39 @@ function customerPrompt(res) {
                         if (err) throw err;
                       }
                     );
-                    if (totalCost % 1 === 0) {
-                      totalCost = `$${totalCost}.00`;
-                    } else if ((10 * totalCost) % 1 === 0) {
-                      totalCost = `$${totalCost}0`;
-                    } else {
-                      totalCost = `$${totalCost}`;
-                    }
-                    console.log(`Thank you for your patronage! Your total is: ${totalCost}`);
-                    connection.end();
+                    connection.query("SELECT price FROM products WHERE ?",
+                      {
+                        item_id: id
+                      },
+                      function(err, res) {
+                        if (err) throw err;
+                        let unitPrice = res[0].price;
+                        // The total cost of the user's purchase is equal to the number of units ordered times the price per unit. Multiplying and then dividing by 100 ensures that the operations are performed on integers, thus avoiding the potential imprecision of floating-point operations.
+                        let totalCost = (100 * orderQuantity * unitPrice) / 100;
+                        connection.query(`UPDATE products SET product_sales = product_sales + ${totalCost} WHERE ?`,
+                          {
+                            item_id: id
+                          },
+                          function(err) {
+                            if (err) throw err;
+                          }
+                        );
+                        if (totalCost % 1 === 0) {
+                          totalCost = `$${totalCost}.00`;
+                        } else if ((10 * totalCost) % 1 === 0) {
+                          totalCost = `$${totalCost}0`;
+                        } else {
+                          totalCost = `$${totalCost}`;
+                        }
+                        console.log(`Thank you for your patronage! Your total is: ${totalCost}`);
+                        connection.end();
+                      }
+                    );
                   }
-                );
-              }
-            }
-          );
-        });
+                }
+              );
+            });
+        }
+      );
     });
 }
